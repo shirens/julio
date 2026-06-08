85b073c761a0c291c97722473830be129c0bd99a
julio-1 <2511500076@mahasiswa.atmaluhur.ac.id>
2026-06-08 13:41:57 +0700

    test commit

--- page/kelas.php
+++ page/kelas.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1 class="m-0 text-dark">Data Kelas</h1>
+                <h1 class="m-0 text-dark">Data KELAS</h1>
             </div>
         </div>
     </div>
